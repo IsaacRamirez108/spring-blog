8e88ebcc46c0d77e1739f8a4180c6780db987169
Isaac Ramirez <isaac.ramirez7365@gmail.com>
2023-03-03 21:03:19 -0600

    Updates to Controller

--- src/main/java/com/codeup/codeupspringblog/RollDiceController.java
+++ src/main/java/com/codeup/codeupspringblog/RollDiceController.java
@@ -11,6 +11,12 @@ public class RollDiceController {
         return "roll-dice/index";
     }
 
-
-
+    @GetMapping("/roll-dice/{number}")
+    public String showResult(@PathVariable int number, Model model) {
+        int result = (int) (Math.random() * 6) + 1;
+        System.out.println((int) 4.9);
+        model.addAttribute("number", number);
+        model.addAttribute("result", result);
+        return "roll-dice/show";
+    }
 }
